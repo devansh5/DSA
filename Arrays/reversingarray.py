3a295b53ed4b331cb3bedc30bdb531c0431938d8
devansh5 <devkumar5436@gmail.com>
2020-05-21 14:22:47 +0530

    Array section added

---
+++ Arrays/reversingarray.py
@@ -0,0 +1,20 @@
+# Time complexity O(n)
+# Space complexity
+
+n=int(input())
+a=list(map(int,input().split()))[:n]
+k=int(input())
+
+def reversearray(a,start,end):
+    while start<end:
+        a[start],a[end-1]=a[end-1],a[start]
+        start+=1
+        end-=1
+    
+def rotateArr(a,k,n):
+    reversearray(a,0,n)
+    reversearray(a,0,n-k)
+    reversearray(a,n-k,n)
+
+
+
